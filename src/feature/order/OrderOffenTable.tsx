d540a738366c630830f2a16819217685fb26badb
iasaad <iasaad@stud.hs-offenburg.de>
2025-06-26 20:13:12 +0200

    Wareneingang für bestellte Menge

--- src/feature/order/OrderOffenTable.tsx
+++ src/feature/order/OrderOffenTable.tsx
@@ -157,6 +157,7 @@ const OrderOffenTable: React.FC<OrderOffenTableProps> = ({ onSelectionChange, se
         const [gesperrtMenge, setGesperrtMenge] = useState<number>(0);
         const [reklamiertMenge, setReklamiertMenge] = useState<number>(0);
         const [menge, setMenge] = useState<number>(order.menge);
+        const [summeUngueltig, setSummeUngueltig] = useState(false);
 
         const [guterSaugfaehigkeit, setGuterSaugfaehigkeit] = useState<number>(0);
         const [guterWeissgrad, setGuterWeissgrad] = useState<number>(0);
@@ -164,10 +165,6 @@ const OrderOffenTable: React.FC<OrderOffenTableProps> = ({ onSelectionChange, se
         const [gesperrtSaugfaehigkeit, setGesperrtSaugfaehigkeit] = useState<number>(0);
         const [gesperrtWeissgrad, setGesperrtWeissgrad] = useState<number>(0);
 
-        useEffect(() => {
-          const sum = guterMenge + gesperrtMenge + reklamiertMenge;
-          setMenge(sum);
-        }, [guterMenge, gesperrtMenge, reklamiertMenge]);
 
         const [updateStatus, { isLoading: isUpdating }] = orderApi.useUpdateMultipleOrdersStatusMutation();
         const [createWareneingang, { isLoading: isCreating }] = wareneingangApi.useCreateWareneingangMutation();
@@ -193,6 +190,12 @@ const OrderOffenTable: React.FC<OrderOffenTableProps> = ({ onSelectionChange, se
             setMenge(order.menge);
           }
         }, [openEditDialog, order]);
+
+        useEffect(() => {
+          const summe = guterMenge + gesperrtMenge + reklamiertMenge;
+          setSummeUngueltig(summe > menge);
+        }, [menge, guterMenge, gesperrtMenge, reklamiertMenge]);
+
 
         const handleSave = async () => {
           try {
@@ -427,7 +430,13 @@ const OrderOffenTable: React.FC<OrderOffenTableProps> = ({ onSelectionChange, se
                     </div>
 
                     <div className="mt-6">
-                      <Button onClick={handleWareneingang}>Anlegen</Button>
+                      
+                      {summeUngueltig && (
+                        <div className="text-red-500 mt-2">
+                          Die Summe aus guter, gesperrter und reklamierter Menge darf die eingetroffene Menge nicht übersteigen.
+                        </div>
+                      )}
+                      <Button onClick={handleWareneingang}  disabled={summeUngueltig}>Anlegen</Button>
                     </div>
                   </DialogContent>
                 </Dialog>
